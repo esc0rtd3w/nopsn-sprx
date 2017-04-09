229edf647b67aa3b782d96787df16b9b57929638
Jason Borowski <esc0rtd3w@gmail.com>
2017-04-09 19:23:02 -0400

    Fixed Typo

--- nopsn/messages.h
+++ nopsn/messages.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #include "core.h"
-//#include "dialog.h"
+#include "dialog.h"
 
-//using namespace Dialog;
+using namespace Dialog;
 
 
 void PrintToXMB(char* text)
 {
-	//msgdialog_mode = MODE_STRING_OK;
-	//Show(text);
+	msgdialog_mode = MODE_STRING_OK;
+	Show(text);
 }
 
 
