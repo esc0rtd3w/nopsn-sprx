d8a3197a8554fa55a5b34e13229e6d210990e9c4
Jason Borowski <esc0rtd3w@gmail.com>
2017-04-17 22:53:38 -0400

    Updated NoPSN Memory Addresses For Apps

--- nopsn/nopsn.h
+++ nopsn/nopsn.h
@@ -67,10 +67,208 @@ int NPUP31359_a[] = {0x0008D628};
 int NPUP31359_o[] = {0x4BFFD421};
 int NPUP31359_v[] = {0x60000000};
 
+// DRTV (8 bytes)
+int NPEB02310_a[] = {0x0025C442};
+u64 NPEB02310_o[] = {0x747275650D};
+u64 NPEB02310_v[] = {0x66616C7365};
+
+// FilmBox Live
+int NPEB01920_a[] = {0x000A5A04};
+int NPEB01920_o[] = {0x4BFFEB0D};
+int NPEB01920_v[] = {0x60000000};
+
+// FilmIn (8 bytes)
+int NPEB02418_a[] = {0x0025E362};
+u64 NPEB02418_o[] = {0x747275650D};
+u64 NPEB02418_v[] = {0x66616C7365};
+
+// Foxtel Play
+int NPEB02003_a[] = {0x0008A06C};
+int NPEB02003_o[] = {0x4BFFEB11};
+int NPEB02003_v[] = {0x60000000};
+
+// FunimationNow
+int NPEB02410_a[] = {0x00094D64};
+int NPEB02410_o[] = {0x4BFFCC99};
+int NPEB02410_v[] = {0x60000000};
+
+// HBO Nordic
+int NPEB02292_a[] = {0x00092A94};
+int NPEB02292_o[] = {0x4BFFD421};
+int NPEB02292_v[] = {0x60000000};
+
+// ICFlix (8 bytes)
+int NPEB02187_a[] = {0x0025C102};
+u64 NPEB02187_o[] = {0x747275650D};
+u64 NPEB02187_v[] = {0x66616C7365};
+
+// IGN (8 bytes)
+int NPUP31341_a[] = {0x001BDDCA};
+u64 NPUP31341_o[] = {0x747275650D};
+u64 NPUP31341_v[] = {0x66616C7365};
+
+// Infinity (8 bytes)
+int NPEB01973_a[] = {0x00267190};
+u64 NPEB01973_o[] = {0x747275650D};
+u64 NPEB01973_v[] = {0x66616C7365};
+
+// Lightbox
+int NPEB02213_a[] = {0x0009A868};
+int NPEB02213_o[] = {0x4BFFD689};
+int NPEB02213_v[] = {0x60000000};
+
+// Maxdome
+int NPEB01192_a[] = {0x00091E93};
+int NPEB01192_o[] = {0x4BFFD3B9};
+int NPEB01192_v[] = {0x60000000};
+
+// MinBio (Mun Kino)
+int NPEB02345_a[] = {0x00093DD0};
+int NPEB02345_o[] = {0x4BFFCCE5};
+int NPEB02345_v[] = {0x60000000};
+
+// Movistar
+int NPEB02135_a[] = {0x00094D64};
+int NPEB02135_o[] = {0x4BFFCC99};
+int NPEB02135_v[] = {0x60000000};
+
+// Multiplayer.it (8 bytes)
+int NPEB01990_a[] = {0x001BBC32};
+u64 NPEB01990_o[] = {0x747275650D};
+u64 NPEB01990_v[] = {0x66616C7365};
+
+// Netflix (app.sprx)
+int NPUP00030_a[] = {0x00017038};
+int NPUP00030_o[] = {0x48983B81};
+int NPUP00030_v[] = {0x60000000};
+
+//int NPUP00030_a[] = {0x00F0D593};
+//u64 NPUP00030_o[] = {0x747275650D};
+//u64 NPUP00030_v[] = {0x66616C7365};
+
+int NPEB00874_a[] = {0x00017038};
+int NPEB00874_o[] = {0x48983B81};
+int NPEB00874_v[] = {0x60000000};
+
+//int NPEB00874_a[] = {0x00F0D593};
+//u64 NPEB00874_o[] = {0x747275650D};
+//u64 NPEB00874_v[] = {0x66616C7365};
+
+int NPJB00766_a[] = {0x00017038};
+int NPJB00766_o[] = {0x48983B81};
+int NPJB00766_v[] = {0x60000000};
+
+//int NPJB00766_a[] = {0x00F0D593};
+//u64 NPJB00766_o[] = {0x747275650D};
+//u64 NPJB00766_v[] = {0x66616C7365};
+
+// NowTV
+int NPEB01919_a[] = {0x00092A94};
+int NPEB01919_o[] = {0x4BFFD421};
+int NPEB01919_v[] = {0x60000000};
+
+// NRK Super
+int NPEB02136_a[] = {0x00093DD0};
+int NPEB02136_o[] = {0x4BFFCCE5};
+int NPEB02136_v[] = {0x60000000};
+
+// NRK TV
+int NPEB02137_a[] = {0x00093DD0};
+int NPEB02137_o[] = {0x4BFFCCE5};
+int NPEB02137_v[] = {0x60000000};
+
+// Pathe Thuis
+int NPEB01974_a[] = {0x00080784, 0x00080994};
+int NPEB01974_o[] = {0x4BFFE0D1, 0x4BFFDEC1};
+int NPEB01974_v[] = {0x60000000, 0x60000000};
+
+// Plus7 by Yahoo!
+int NPEB02161_a[] = {0x00094D64};
+int NPEB02161_o[] = {0x4BFFCC99};
+int NPEB02161_v[] = {0x60000000};
+
+// QuickFlix
+int NPEB00864_a[] = {0x0009D2B0};
+int NPEB00864_o[] = {0x4BFFD3B9};
+int NPEB00864_v[] = {0x60000000};
+
+// SBS On Demand
+int NPEB02050_a[] = {0x00093DD0};
+int NPEB02050_o[] = {0x4BFFCCE5};
+int NPEB02050_v[] = {0x60000000};
+
+// Skai (8 bytes)
+int NPEB01987_a[] = {0x001A68CA};
+u64 NPEB01987_o[] = {0x747275650D};
+u64 NPEB01987_v[] = {0x66616C7365};
+
+// Sky Ticket
+int NPEB02339_a[] = {0x0009348C};
+int NPEB02339_o[] = {0x4BFFD3B9};
+int NPEB02339_v[] = {0x60000000};
+
+// Spotify
+int NPEB02222_a[] = {0x000B75E7};
+int NPEB02222_o[] = {0x4BFFD611};
+int NPEB02222_v[] = {0x60000000};
+
+//int NPEB02222_a[] = {0x0026A102};
+//u64 NPEB02222_o[] = {0x747275650D};
+//u64 NPEB02222_v[] = {0x66616C7365};
+
+// Total Channel
+int NPEB01848_a[] = {0x0009CCD0};
+int NPEB01848_o[] = {0x4BFFD499};
+int NPEB01848_v[] = {0x60000000};
+
 // TuneIn Radio
 int NPUP10042_a[] = {0x0000B700, 0x0000B708, 0x0000BB20};
 int NPUP10042_o[] = {0x4BFFF7C1, 0x408203EC, 0x4BFFF3A1};
 int NPUP10042_v[] = {0x60000000, 0x60000000, 0x60000000};
+
+// TV 2 Sumo (8 bytes)
+int NPEB01956_a[] = {0x0025FD6A};
+u64 NPEB01956_o[] = {0x747275650D};
+u64 NPEB01956_v[] = {0x66616C7365};
+
+// TV3 (8 bytes)
+int NPEB02166_a[] = {0x001C29D2};
+u64 NPEB02166_o[] = {0x747275650D};
+u64 NPEB02166_v[] = {0x66616C7365};
+
+// VEVO (8 bytes)
+int NPEB02212_a[] = {0x001D901A};
+u64 NPEB02212_o[] = {0x747275650D};
+u64 NPEB02212_v[] = {0x66616C7365};
+
+// ViaPlay
+int NPEB00863_a[] = {0x0009CCD0};
+int NPEB00863_o[] = {0x4BFFD3B9};
+int NPEB00863_v[] = {0x60000000};
+
+// VUDU (last address is Region-Free)
+int NPUP00047_a[] = {0x00003868, 0x005AAD1C, 0x0000B450};
+int NPUP00047_o[] = {0x4BFFF83D, 0x4BA58389, 0x419E0034};
+int NPUP00047_v[] = {0x60000000, 0x60000000, 0x60000000};
+
+// Wuaki.tv
+int NPEB01813_a[] = {0x00094D64};
+int NPEB01813_o[] = {0x4BFFCC99};
+int NPEB01813_v[] = {0x60000000};
+
+// YLE Areena
+//int NPEB02338_a[] = {0x000B7734};
+//int NPEB02338_o[] = {0x4BFFD53D};
+//int NPEB02338_v[] = {0x60000000};
+
+int NPEB02338_a[] = {0x0025C412};
+u64 NPEB02338_o[] = {0x747275650D};
+u64 NPEB02338_v[] = {0x66616C7365};
+
+// Yomvi
+//int NPEB02135_a[] = {0x0009A868};
+//int NPEB02135_o[] = {0x4BFFD689};
+//int NPEB02135_v[] = {0x60000000};
 
 // YouTube
 int NPUP10028_a[] = {0x00065F30};
@@ -84,4 +282,9 @@ int NPEB01229_v[] = {0x60000000};
 int NPJB00286_a[] = {0x00065F30};
 int NPJB00286_o[] = {0x4BFFFEDD};
 int NPJB00286_v[] = {0x60000000};
+
+// YuppTV (8 bytes)
+int NPEB02350_a[] = {0x0025CE32};
+u64 NPEB02350_o[] = {0x747275650D};
+u64 NPEB02350_v[] = {0x66616C7365};
 
