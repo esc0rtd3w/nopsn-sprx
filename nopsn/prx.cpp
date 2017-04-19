14df151aa4ca1f8fe591065970b493615a2744e3
Jason Borowski <esc0rtd3w@gmail.com>
2017-04-19 02:24:24 -0400

    Cleaned Up GetCID() Function [Now getting Full CID In Hex]

--- nopsn/prx.cpp
+++ nopsn/prx.cpp
@@ -106,7 +106,7 @@ void thread_nopsn(uint64_t arg)
 				// YouTube
 				GetPatchValues(NPUP10028);
 
-				PrintDebugStats(LONG);
+				PrintDebugStats(SHORT);
 				
 				/*
 				msgdialog_mode = MODE_STRING_YESNO;
